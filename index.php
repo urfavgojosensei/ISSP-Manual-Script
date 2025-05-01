50f095494047b55e0a7ba501f56efe1732c23b2b
jsonflorendo <jsonflorendo@gmail.com>
2025-05-01 14:30:56 +0800

    my 2nd commit

--- index.php
+++ index.php
@@ -47,7 +47,11 @@
 
       else{
         ?>
+<<<<<<< Updated upstream
         <h1 align="center">Something went wrong! 1212125</h1>
+=======
+        <h1 align="center">55555 Something went wrong! 121212</h1>
+>>>>>>> Stashed changes
         <?php
       }
     ?>
